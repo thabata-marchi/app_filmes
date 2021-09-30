0f1674c35aa8b3a2379aaa906d8a1e7e0b1be8cb
Thabata Marchi <tatha.marchi@gmail.com>
2021-09-29 23:48:04 -0300

    Create navigation of tabBar

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,9 +1,14 @@
 import 'package:app_filmes/application/modules/module.dart';
+import 'package:app_filmes/modules/home/home_bindings.dart';
 import 'package:app_filmes/modules/home/home_page.dart';
 import 'package:get/get.dart';
 
 class HomeModule extends Module{
     List<GetPage> routers = [
-      GetPage(name: '/home', page: () => HomePage(),)
+      GetPage(
+        name: '/home', 
+        page: () => HomePage(),
+        binding: HomeBindings()
+      )
     ];
 }
